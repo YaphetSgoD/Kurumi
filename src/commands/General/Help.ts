5f10e6aa66f3dab78485c68bdeb86a74240fe011
Examinati <72595444+Eximinati@users.noreply.github.com>
2022-03-23 00:10:29 +0500

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -19,7 +19,7 @@ export default class Command extends BaseCommand {
 
     run = async (M: ISimplifiedMessage, parsedArgs: IParsedArgs): Promise<void> => {
             const n = [
-            './assets/Pikachu/Pikachu.mp4'
+            './assets/Pikachu/_yuno.mirai_-20220322-0001.mp4'
         ]
         let rin = n[Math.floor(Math.random() * n.length)]
         if (!parsedArgs.joined) {
